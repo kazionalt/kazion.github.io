0cc1887ebfefb7884931db787b4ee4df1bac87d2
kazionalt <70506328+kazionalt@users.noreply.github.com>
2020-08-31 02:50:38 -0400

    Delete client.js

--- client.js
+++
@@ -1,8 +0,0 @@
-fetch('/json')
-  .then(response => {
-    console.log('response', response)
-    return response.json()
-  })
-  .then(response => {
-    document.querySelector('#ip').innerText = response.ip
-  })
